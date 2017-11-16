0167c2c27bc923bda29e67e2854d3a22b860c025
Taha Abbasi <tahaabbasi@me.com>
2017-11-16 09:57:16 -0500

    Promise Demo completed

--- example-promise.js
+++ example-promise.js
@@ -1,16 +1,52 @@
-function doWork (data, callback) {
-    callback('done');
-}
+// function doWork (data, callback) {
+//     callback('done');
+// }
+//
+// function doWorkPromise (data) {
+//     return new Promise(function (resolve, reject) {
+//         resolve('everything worked!');
+//         // reject({
+//         //     error: 'something bad happened'
+//         // });
+//     });
+// }
+//
+// doWorkPromise('some data').then(function (data) {
+//     console.log(data);
+// });
+//
 
-function doWorkPromise (data) {
+var request = require('request');
+
+function getWeather (location) {
     return new Promise(function (resolve, reject) {
-        resolve('everything worked!');
-        // reject({
-        //     error: 'something bad happened'
-        // });
+        var encodedLocation = encodeURIComponent(location);
+        var url = 'http://api.openweathermap.org/data/2.5/weather?appid=7e3f1dd378bef34937935f0128e0a29c&q=' + encodedLocation + '&type=like&units=imperial';
+
+
+        if (!location) {
+            return reject('No location provided');
+        }
+
+        request({
+            url: url,
+            json: true
+        }, function (error, response, body) {
+            if (error) {
+                reject('Unable to fetch weather');
+            } else {
+                if (body.cod === '404') {
+                    return callback(body.message);
+                }
+                // console.log(JSON.stringify(body, null, 4));
+                resolve('It\'s ' + body.main.temp + ' in ' + body.name + '!');
+            }
+        });
     });
 }
 
-doWorkPromise('some data').then(function (data) {
-    console.log(data);
+getWeather('Philadelphia').then(function (currentWeather) {
+    console.log(currentWeather);
+}, function (error) {
+    console.log(error);
 });
